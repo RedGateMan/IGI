573680e086d949da42178479c4bbc4f70ebcf08b
RedGateMan <egor.chijov@gmail.com>
2023-02-21 00:08:41 +0300

    Resloving Cherry-pick

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,35 +1,39 @@
 from helpers import Operations
 
 print("Hello, World!")
-first_int = int(input("Enter A: "))
-second_int = int(input("Enter B: "))
-operation = input("Chose operation (add, sub, mult, div): ")
 
 
 def calc(first_int, second_int, operation):
-    match (operation):
-        case (Operations.ADD.value):
-            print("Sum result: ", first_int + second_int)
-        case (Operations.SUB.value):
-            print("Sub result(first_int - second_int): ", first_int - second_int)
-        case (Operations.MULT.value):
-            print("Mul result: ", first_int * second_int)
-        case (Operations.DIV.value):
-            try:
-                print("Div result(first_int / second_int): ", first_int / second_int)
-            except ZeroDivisionError:
-                print("Division by zero!")
-        case _:
-            print("Invalid input!")
+	match (operation):
+		case (Operations.ADD):
+			print("Sum result: ", first_int + second_int)
+		case (Operations.SUB):
+			print("Sub result(first_int - second_int): ", first_int - second_int)
+		case (Operations.MULT):
+			print("Mul result: ", first_int * second_int)
+		case (Operations.DIV):
+			try:
+				print("Div result(first_int / second_int): ", first_int / second_int)
+			except ZeroDivisionError:
+				print("Division by zero!")
 
 
-calc(first_int, second_int, operation)
+try:
+	first_int = int(input("Enter A: "))
+	second_int = int(input("Enter B: "))
+	operation = Operations(input("Chose operation (add, sub, mult, div): "))
+	calc(first_int, second_int, operation)
+except ValueError:
+	print("Invalid input!")
 
 
-def even_list():
-    array = list(map(int, input("Input your array(divided by spaces)\n").split()))
-    even_array = [x for x in array if x % 2 == 0]
-    return even_array
+def even_list(array):
+	even_array = [x for x in array if x % 2 == 0]
+	return even_array
 
 
-print(even_list())
+try:
+	array = list(map(int, input("Input your array(divided by spaces)\n").split()))
+	print(even_list(array))
+except ValueError:
+	print("Invalid input!")
